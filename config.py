7490933e1fc275a1743faa9fadd3b2d892c0aa8f
Ngrover24198 <ngrover.241@gmail.com>
2024-03-20 01:17:15 +0530

    comments added in all the files

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 
+# this is the configuration file, that's usually helpful in managing multiple environment variables and definition
 class Config(object):
     # OpenAI API Key
     OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from app import app, db
 
+
+# this file helps in running the actual application
 if __name__ == '__main__':
     app.run(debug=True)
